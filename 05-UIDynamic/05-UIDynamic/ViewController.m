ccfc6185d1500fd7222f89d3e2531747cceeb9e9
zyc <10168363742qq.com>
2017-06-27 18:23:26 +0800

    吸附/捕捉行为
    注意吸附行为创建时就需添加控件和吸附点、一个控件一次性只能有一个吸附点、防抖系数

--- 05-UIDynamic/05-UIDynamic/ViewController.m
+++ 05-UIDynamic/05-UIDynamic/ViewController.m
@@ -37,45 +37,50 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
+    //获得触摸点
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.view];
+
+    //1.创建 吸附/捕捉行为
+    UISnapBehavior *snap = [[UISnapBehavior alloc]initWithItem:self.blueView snapToPoint:point];
+    //防抖系数（值越小越抖）
+    snap.damping = 0.2;
+    //2.添加行为
+    [self.animator removeAllBehaviors];//因为一个控件一次性只能吸附一个点
+    [self.animator addBehavior:snap];
+}
+
+- (void)testCollision
+{
     //1.创建 碰撞行为
     UICollisionBehavior *collision = [[UICollisionBehavior alloc]init];
     //添加边界
     
-    
-    CGFloat startY = self.view.frame.size.height*0.5;
-    CGFloat endX = self.view.frame.size.width;
-    CGFloat endY = self.view.frame.size.height;
-//    [collision addBoundaryWithIdentifier:@"line" fromPoint:CGPointMake(0, startY) toPoint:CGPointMake(endX, endY)];
-//    [collision addBoundaryWithIdentifier:@"line2" fromPoint:CGPointMake(endX, 0) toPoint:CGPointMake(endX, endY)];
+    //    CGFloat startY = self.view.frame.size.height*0.5;
+    //    CGFloat endX = self.view.frame.size.width;
+    //    CGFloat endY = self.view.frame.size.height;
+    //    [collision addBoundaryWithIdentifier:@"line" fromPoint:CGPointMake(0, startY) toPoint:CGPointMake(endX, endY)];
+    //    [collision addBoundaryWithIdentifier:@"line2" fromPoint:CGPointMake(endX, 0) toPoint:CGPointMake(endX, endY)];
     
     CGFloat width = self.view.frame.size.width;
     UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, width, width)];
-    
     [collision addBoundaryWithIdentifier:@"circle" forPath:path];
     
     //让参照视图的bounds变为碰撞检测的边框
-//    collision.translatesReferenceBoundsIntoBoundary = YES;
+    //    collision.translatesReferenceBoundsIntoBoundary = YES;
     
     [collision addItem:self.blueView];
-//    [collision addItem:self.segment];
+    //    [collision addItem:self.segment];
     //2.创建 重力行为
     UIGravityBehavior * gravity = [[UIGravityBehavior alloc]init];
-//    gravity.gravityDirection = CGVectorMake(50, 50);
+    //    gravity.gravityDirection = CGVectorMake(50, 50);
     gravity.magnitude = 100;
     [gravity addItem:self.blueView];
-  
-    
-    
-    
     //3.添加行为
     [self.animator addBehavior:collision];
     [self.animator addBehavior:gravity];
     
-    
-    
-    
 }
-
 - (void)testGravity
 {
     //1.创建物理仿真行为 - 重力行为
