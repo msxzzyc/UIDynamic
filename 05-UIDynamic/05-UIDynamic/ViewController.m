aec38855dc181adee8fc23c75a72c3f441009f69
zyc <10168363742qq.com>
2017-06-27 13:27:55 +0800

    重力行为仿真
    创建物理仿真器和物理仿真行为，将仿真行为添加到仿真器中。注意将物理仿真器添加为属性，及重力行为方向和加速度。

--- 05-UIDynamic/05-UIDynamic/ViewController.m
+++ 05-UIDynamic/05-UIDynamic/ViewController.m
@@ -9,20 +9,40 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *blueView;
+/** 物理仿真器 */
+@property (strong, nonatomic) UIDynamicAnimator *animator;
 
 @end
 
 @implementation ViewController
 
+- (UIDynamicAnimator *)animator
+{
+    if (!_animator) {//1.创建物理仿真器（ReferenceView：参照视图，即仿真范围）
+        _animator = [[UIDynamicAnimator alloc]initWithReferenceView:self.view];
+    }
+    return _animator;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    
+    
+    //2.创建物理仿真行为 - 重力行为
+    UIGravityBehavior * gravity = [[UIGravityBehavior alloc]init];
+    [gravity addItem:self.blueView];
+    gravity.gravityDirection = CGVectorMake(50, 50);
+    gravity.magnitude = 50;//100point/s² 移动距离 = 1/2 *manitude*时间²
+    
+    //3.添加物理仿真行为到物理仿真器中，开始物理仿真
+    [self.animator addBehavior:gravity];
 }
 
 
