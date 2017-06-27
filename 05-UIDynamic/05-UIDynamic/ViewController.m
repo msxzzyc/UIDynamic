28bbb35220896a0453fa95a9105f14fccc44f1e2
zyc <10168363742qq.com>
2017-06-27 17:47:19 +0800

    碰撞行为
    注意添加边界（包括用bezierPath画圆等）、用rotation设置碰撞方向、_name不兼容懒加载

--- 05-UIDynamic/05-UIDynamic/ViewController.m
+++ 05-UIDynamic/05-UIDynamic/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *blueView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segment;
+
 /** 物理仿真器 */
 @property (strong, nonatomic) UIDynamicAnimator *animator;
 
@@ -19,7 +21,7 @@
 
 - (UIDynamicAnimator *)animator
 {
-    if (!_animator) {//1.创建物理仿真器（ReferenceView：参照视图，即仿真范围）
+    if (!_animator) {//创建物理仿真器（ReferenceView：参照视图，即仿真范围）
         _animator = [[UIDynamicAnimator alloc]initWithReferenceView:self.view];
     }
     return _animator;
@@ -27,23 +29,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //旋转角度
+    self.blueView.transform = CGAffineTransformMakeRotation(M_PI_4);
     
 }
 
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
+    //1.创建 碰撞行为
+    UICollisionBehavior *collision = [[UICollisionBehavior alloc]init];
+    //添加边界
     
     
-    //2.创建物理仿真行为 - 重力行为
+    CGFloat startY = self.view.frame.size.height*0.5;
+    CGFloat endX = self.view.frame.size.width;
+    CGFloat endY = self.view.frame.size.height;
+//    [collision addBoundaryWithIdentifier:@"line" fromPoint:CGPointMake(0, startY) toPoint:CGPointMake(endX, endY)];
+//    [collision addBoundaryWithIdentifier:@"line2" fromPoint:CGPointMake(endX, 0) toPoint:CGPointMake(endX, endY)];
+    
+    CGFloat width = self.view.frame.size.width;
+    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, width, width)];
+    
+    [collision addBoundaryWithIdentifier:@"circle" forPath:path];
+    
+    //让参照视图的bounds变为碰撞检测的边框
+//    collision.translatesReferenceBoundsIntoBoundary = YES;
+    
+    [collision addItem:self.blueView];
+//    [collision addItem:self.segment];
+    //2.创建 重力行为
+    UIGravityBehavior * gravity = [[UIGravityBehavior alloc]init];
+//    gravity.gravityDirection = CGVectorMake(50, 50);
+    gravity.magnitude = 100;
+    [gravity addItem:self.blueView];
+  
+    
+    
+    
+    //3.添加行为
+    [self.animator addBehavior:collision];
+    [self.animator addBehavior:gravity];
+    
+    
+    
+    
+}
+
+- (void)testGravity
+{
+    //1.创建物理仿真行为 - 重力行为
     UIGravityBehavior * gravity = [[UIGravityBehavior alloc]init];
     [gravity addItem:self.blueView];
     gravity.gravityDirection = CGVectorMake(50, 50);
     gravity.magnitude = 50;//100point/s² 移动距离 = 1/2 *manitude*时间²
     
-    //3.添加物理仿真行为到物理仿真器中，开始物理仿真
+    //2.添加物理仿真行为到物理仿真器中，开始物理仿真
     [self.animator addBehavior:gravity];
+    
 }
-
 
 @end
